4ce66934174473d0d1c90c023109abec2e13e321
P4bloHT <106360604+P4bloHT@users.noreply.github.com>
2022-12-15 17:20:10 -0500

    Add files via upload

---
+++ Avance 1.5- Maestro/comunicaciones maestro.d
@@ -0,0 +1,4 @@
+.\comunicaciones maestro.o: comunicaciones maestro.c
+.\comunicaciones maestro.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\comunicaciones maestro.o: tm4c123gh6pm.h
+.\comunicaciones maestro.o: Nokia5110.h
